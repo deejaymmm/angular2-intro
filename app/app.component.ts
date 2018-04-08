2ec69da03933e770fbb31522e994deee8739c945
deejay_mmm <djmmm@ukr.net>
2018-04-08 13:41:43 +0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { Todo } from './shared/todo';
+//import { todos } from './shared/data';
+
+@Component({
+    moduleId: module.id, 
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'] 
+})
+export class AppComponent {
+    title: string = 'Angular 2Do';
+}
---
+++ app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+//import { HttpClientModule } from '@angular/common/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/data.service';
+
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoFormComponent } from './todos/todo-form/todo-form.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoItemComponent } from './todos/todo-item/todo-item.component';
+import { TodoService } from './shared/todo.service';
+
+@NgModule({
+    imports: [
+        BrowserModule, 
+        FormsModule,
+        HttpModule,
+        //HttpClientModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
+    ],
+    declarations: [
+        AppComponent,
+        TodosComponent,        
+        TodoFormComponent,
+        TodoListComponent,
+        TodoItemComponent
+    ],
+    providers: [TodoService],
+    bootstrap: [AppComponent]
+})
+export class AppModule{
+    
+}
